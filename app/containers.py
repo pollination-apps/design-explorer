439c85611f9f97a15ed8e5240c4ca0bc987bdf77
Mikkel Pedersen <mkklpdrsn@gmail.com>
2024-05-16 13:00:58 +0200

    fix(add): Add another sample project

--- app/containers.py
+++ app/containers.py
@@ -1,9 +1,13 @@
 """Module with function to create containers for the app layout."""
+import numpy as np
+import plotly.express as px
+from pathlib import Path
+from typing import List
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 
 
-def logo_title(app):
+def logo_title(app) -> html.Div:
     """Function to create the Div that containers the Pollination logo and app
     title (Design Explorer)."""
     container = html.Div(children=[
@@ -19,7 +23,7 @@ def logo_title(app):
     return container
 
 
-def create_radio_container():
+def create_radio_container() -> html.Div:
     """Function to create the radio items."""
     container = html.Div(
         children=[
@@ -39,8 +43,32 @@ def create_radio_container():
     return container
 
 
-def color_parallel_coordinates(parameters, color_by):
+def select_sample_project() -> html.Div:
     """Function to create the Div that contains the options for coloring the
+    parallel coordinates by a column."""
+    children = []
+    children.append(dbc.DropdownMenuItem('Sample', id={'select_sample_project': 'sample'}))
+    children.append(dbc.DropdownMenuItem('Daylight Factor', id={'select_sample_project': 'daylight-factor'}))
+    dropdown_menu = dbc.DropdownMenu(
+        id='select-sample-dropdown',
+        label='Sample',
+        children=children,
+        direction='end'
+    )
+
+    select_sample_label = html.Label(children='Select sample', className='color-by-label')
+
+    select_sample_container = html.Div(
+        className='color-by',
+        id='select-sample',
+        children=[select_sample_label, dropdown_menu]
+    )
+
+    return select_sample_container
+
+
+def create_color_by_children(parameters, color_by) -> List[html.Div]:
+    """Function to create the children for the options for coloring the
     parallel coordinates by a column."""
     children = []
     children_input = []
@@ -74,16 +102,26 @@ def color_parallel_coordinates(parameters, color_by):
     store = dcc.Store(id='color-by-column', data=color_by)
     color_by_label = html.Label(children='Color by', className='color-by-label')
 
+    children = [color_by_label, dropdown_menu, store]
+
+    return children
+
+
+def create_color_by_container(parameters, color_by) -> html.Div:
+    """Function to create the Div that contains the options for coloring the
+    parallel coordinates by a column."""
+    children = create_color_by_children(parameters, color_by)
+
     color_by_container = html.Div(
         className='color-by',
         id='color-by',
-        children=[color_by_label, dropdown_menu, store]
+        children=children
     )
 
     return color_by_container
 
 
-def sort_images(parameters, sort_by):
+def create_sort_by_children(parameters, sort_by) -> html.Div:
     """Function to create the Div that contains the options for sorting the
     images in the grid."""
     children = []
@@ -124,17 +162,59 @@ def sort_images(parameters, sort_by):
                                id='sort-by-label',
                                className='sort-by-label')
 
+    children = [sort_by_label, dropdown_menu, button_ascending, sort_by_store,
+                sort_ascending_store]
+
+    return children
+
+
+def create_sort_by_container(parameters, sort_by) -> html.Div:
+    """Function to create the Div that contains the options for sorting the
+    images in the grid."""
+    children = create_sort_by_children(parameters, sort_by)
+
     sort_container = html.Div(
-        children=[sort_by_label, dropdown_menu, button_ascending, sort_by_store,
-                  sort_ascending_store],
+        children=children,
         className='sort-by',
-        id='sort-by-div'
+        id='sort-by'
     )
 
     return sort_container
 
 
-def images(images_div):
+def create_images_grid_children(
+        sorted_df_records, color_by, minimum, maximum, img_column,
+        project_folder) -> List[html.Div]:
+    children = []
+    project_folder = Path(project_folder)
+    for record in sorted_df_records:
+        samplepoints = np.interp(record[color_by], [minimum, maximum], [0, 1])
+        border_color = px.colors.sample_colorscale(
+            'plasma', samplepoints=samplepoints
+        )[0]
+        src = project_folder.joinpath(record[img_column])
+        image = html.Div(
+            html.Img(src=src.as_posix(),
+                        id={'image': f'{record[img_column]}'},
+                        className='image-grid',
+                        style={'border-color': border_color}
+            ),
+            style={
+                'aspect-ratio': '1',
+                'width': '100%',
+                'height': '100%',
+                'position': 'relative',
+                'display': 'flex',
+                'align-items': 'center',
+                'justify-content': 'center',
+                }
+        )
+        children.append(image)
+
+    return children
+
+
+def create_images_container(images_div) -> html.Div:
     """Function to create a Div for images."""
     images_container = html.Div([
         dcc.Store(id='selected-image-data'),
