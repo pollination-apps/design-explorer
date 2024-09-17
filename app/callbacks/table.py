1a960997757b830d0f8fde7b8ebd3a1d0a4752fb
Mikkel Pedersen <mkklpdrsn@gmail.com>
2024-09-17 20:56:59 +0200

    fix(callbacks): Move callback functions to separate modules

---
+++ app/callbacks/table.py
@@ -0,0 +1,12 @@
+import dash
+from dash.dependencies import Input, Output
+
+
+@dash.callback(
+    Output('table', 'data', allow_duplicate=True),
+    Input('active-records', 'data'),
+    prevent_initial_call=True,
+)
+def update_table_data(active_records):
+    """If the active-records is changed, the data will be updated in table."""
+    return active_records
